7949373d35f296fac47a3932fad0cccb4a72b776
olakunle-exxdee <saheedogunsolu2016@gmail.com>
2023-06-11 15:33:59 +0200

    sundy7

--- render/views.py
+++ render/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import Render
 
 
 # Create your views here.
 def index(request):
-    return render(request, "render/index.html", {})
+    obj = Render(name="olakune", description="ola")
+    obj.save()
+    ans = Render.objects.all().values()
+    print(request)
+    return render(
+        request,
+        "render/index.html",
+    )
