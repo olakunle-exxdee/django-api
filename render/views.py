118b6b8fd7086d5454ae06dc6182d9b8ad1411ba
olakunle-exxdee <saheedogunsolu2016@gmail.com>
2023-06-17 10:44:34 +0200

    let goo

--- render/views.py
+++ render/views.py
@@ -4,11 +4,8 @@ from .models import Render
 
 # Create your views here.
 def index(request):
-    obj = Render(name="olakune", description="ola")
+    obj = Render(name="olakunle", description="lorem")
     obj.save()
     ans = Render.objects.all().values()
     print(request)
-    return render(
-        request,
-        "render/index.html",
-    )
+    return render(request, "render/index.html", {"product": ans})
