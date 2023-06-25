5cef56245b1a54ac980fc5987315aa6754bea4cc
olakunle-exxdee <saheedogunsolu2016@gmail.com>
2023-06-25 10:34:17 +0200

    Revert "let goo"
    This reverts commit 118b6b8fd7086d5454ae06dc6182d9b8ad1411ba.

--- render/views.py
+++ render/views.py
@@ -4,8 +4,15 @@ from .models import Render
 
 # Create your views here.
 def index(request):
-    obj = Render(name="olakunle", description="lorem")
+    obj = Render(name="olakune", description="ola")
     obj.save()
     ans = Render.objects.all().values()
     print(request)
+<<<<<<< HEAD
     return render(request, "render/index.html", {"name": list(ans)})
+=======
+    return render(
+        request,
+        "render/index.html",
+    )
+>>>>>>> parent of 118b6b8 (let goo)
